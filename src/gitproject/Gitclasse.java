895298bd708e179499726b999b82cb1509b099e0
omayma-chakkour <oouummaa.sh@gmail.com>
2022-02-11 14:07:49 +0100

    changing hafida's code

--- src/gitproject/Gitclasse.java
+++ src/gitproject/Gitclasse.java
@@ -3,7 +3,7 @@ package gitproject;
 public class Gitclasse {
 	public static void main(String[] args) {
 	       System.out.print("hello world! hafida");
-		   System.out.println("oumaima's try");
+		   System.out.println("oumaima's try ??");
 	    
 	}
 
