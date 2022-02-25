f61765786a78f54f42c77ec3f5b0d10a778fc881
jakoum <84620488+jakoum@users.noreply.github.com>
2022-02-25 11:14:29 +0100

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+package devops;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.print("first code");
+		System.out.print("first modification to code");
+		System.out.print("second modification modification to code");
+		System.out.print("third modification modification to code");
+		
+	}
+
+}
